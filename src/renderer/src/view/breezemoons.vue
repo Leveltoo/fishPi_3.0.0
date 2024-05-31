74c46cdca352f64817a0dec32f464eaea864333b
wanghc <1056480375@qq.com>
2024-05-31 16:59:55 +0800

    add: 字体文件处理

--- src/renderer/src/view/breezemoons.vue
+++ src/renderer/src/view/breezemoons.vue
@@ -1,5 +1,60 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Avatar, Button, Icon, Input, Message, Time } from 'view-ui-plus'
+import { onMounted, reactive } from 'vue'
+import FishPi from 'fishpi'
 
-<template></template>
+const fishPi = new FishPi()
+const content = reactive({
+  list: [],
+  page: 1,
+  message: '',
+  sending: false
+})
+const formatContent = (content) => {
+  return content.replace(/<a\s+(.*?)>/g, '<a $1 target="_blank">')
+}
+onMounted(async () => {
+  const res = await fishPi.breezemoon.list(content.page)
+  if (res.code !== 0) {
+    console.log(res)
+    Message.error(res.msg)
+  } else {
+    content.list = res.breezemoons
+  }
+})
+</script>
 
-<style scoped></style>
+<template>
+  <div class="container">
+    <div class="top">
+      <Input placeholder="清风明月">
+        <template #append>
+          <Button :loading="content.sending" icon="md-send"></Button>
+        </template>
+      </Input>
+    </div>
+    <div class="content">
+      <div v-for="(item, index) in content.list" :key="index" class="item">
+        <Avatar
+          :data-user="item.breezemoonAuthorName"
+          :src="item.breezemoonAuthorThumbnailURL48"
+          :title="item.breezemoonAuthorName"
+          class="msg-avatar user-card"
+        />
+        <div class="item_right">
+          <div class="arrow" />
+          <p class="item_right_content" v-html="formatContent(item.breezemoonContent)"></p>
+          <div class="item_right_time">
+            发自{{ item.breezemoonCity }}
+            <Time :time="item.breezemoonCreated"></Time>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="more">
+      <Icon type="md-arrow-dropdown" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
