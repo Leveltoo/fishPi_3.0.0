67c23a80e4e3b53a3cfc14d0a90a19f56045b5d2
wanghc <1056480375@qq.com>
2024-06-03 11:06:42 +0800

    add: 调整header组件样式以及图片本地资源显示

--- src/renderer/src/components/Header.vue
+++ src/renderer/src/components/Header.vue
@@ -1,14 +1,35 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
 import useStore from '../store'
-import { Header } from 'view-ui-plus'
+import { Button, Header, Icon } from 'view-ui-plus'
+import Music from './Music.vue'
 
 const { setting } = useStore()
 const title = computed(() => `${setting.title}`)
 </script>
 
 <template>
-  <Header class="header">{{ title ? `摸鱼派-${title}` : '摸鱼派' }}</Header>
+  <Header class="header">
+    <h1>
+      <img alt="" src="../assets/icon.png" />
+      <span>{{ title ? `摸鱼派-${title}` : '摸鱼派' }}</span>
+    </h1>
+    <Music></Music>
+    <div class="control">
+      <Button class="button" type="text">
+        <Icon custom="fa fa-minus" />
+      </Button>
+      <Button class="button" type="text">
+        <div class="cirle-empty"></div>
+      </Button>
+      <Button class="button" type="text">
+        <Icon custom="fa fa-thumb-tack" />
+      </Button>
+      <Button class="button" type="text">
+        <Icon custom="fa fa-times" />
+      </Button>
+    </div>
+  </Header>
 </template>
 
 <style lang="scss" scoped>
